e53eedc4193a9fd07d01e47eaaa0b6504e4e9614
Filipe Bacof <filipebacof@gmail.com>
2022-09-04 23:43:09 -0300

    commit

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-/* etapas votação:
+/**
+ *  @version 1.0
+ * @author Filipe Bacof
+ * @description Móddulo da urna eletrônica.
+ etapas votação:
 deputado federal 4 numeros
 deputado estadual 5 numeros
 senador 3 numeros
 governador 2 numeros
 presidente 2 numeros
+
+jsdoc.org
 */
